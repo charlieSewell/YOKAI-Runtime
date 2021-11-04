5afcd97bb6dabc5d8f698aca14e4f885cb324ac0
Charlie Sewell <charliesewell@live.com>
2021-11-04 11:50:24 +0800

    Physics Demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,12 @@
 
 #include "Engine/Yokai.hpp"
 #include "DemoScene/DemoScene.hpp"
-#include "AIScene/AIScene.hpp"
-#include "PhysicsScene/PhysicsScene.hpp"
-#include "LightingScene/LightingScene.hpp"
+#include "CharliePhysics/CharliePhysics.hpp"
 int main()
 {
 	auto& engine = Yokai::getInstance();
 
-	engine.AddScene(std::shared_ptr<Scene>(new DemoScene()));
-	engine.AddScene(std::shared_ptr<Scene>(new AIScene()));
-	engine.AddScene(std::shared_ptr<Scene>(new LightingScene()));
-	engine.AddScene(std::shared_ptr<Scene>(new PhysicsScene()));
+	engine.AddScene(std::shared_ptr<Scene>(new CharliePhysics()));
 
     engine.Run();
 	return 0;
